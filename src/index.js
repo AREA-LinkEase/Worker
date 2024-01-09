32dd11af5cc2ebd29837c26bc34e09048ea4d423
IKEA.luddes <clement.pecas33@gmail.com>
2024-01-09 08:23:17 +0100

    [Feat] (project) : add logs

--- src/index.js
+++ src/index.js
@@ -5,35 +5,45 @@ dotenv.config();
 
 let url = process.env.URL_API
 
-setTimeout(async () => {
-  let response = await fetch(url + "/worker/@next", {
-    headers: {
-      "authorization": process.env.WORKER_API
+setInterval(async () => {
+  console.log("search new Automate")
+  try {
+    let response = await fetch(url + "/worker/@next", {
+      headers: {
+        "authorization": process.env.WORKER_API
+      }
+    })
+    if (!response.ok) {
+      console.log("Error request, code : " + response.status)
+      return;
     }
-  })
-  if (!response.ok) return;
-  let json = await response.json();
+    console.log("start executing an Automate ...")
+    let json = await response.json();
 
-  for (const user of json.users) {
-    let automate = new Automate({
-      "workflow": json.automate.workflow,
-      "tokens": user.tokens
-    }, user.id)
+    console.log("Automate name : " + json.automate.title)
+    for (const user of json.users) {
+      let automate = new Automate({
+        "workflow": json.automate.workflow,
+        "tokens": user.tokens
+      }, user.id)
 
-    await automate.start();
+      await automate.start();
 
-    let logs = automate.getLogs();
+      let logs = automate.getLogs();
 
-    if (logs.length > 0) {
-      await fetch(url + "/worker/automate/" + json.automate.id + "/logs", {
-        headers: {
-          "authorization": process.env.WORKER_API
-        },
-        method: "PUT",
-        body: {
-          logs
-        }
-      })
+      if (logs.length > 0) {
+        await fetch(url + "/worker/automate/" + json.automate.id + "/logs", {
+          headers: {
+            "authorization": process.env.WORKER_API
+          },
+          method: "PUT",
+          body: {
+            logs
+          }
+        })
+      }
     }
+  } catch (e) {
+    console.log(e)
   }
 }, 1000)
